9fd3ae75e3f5a8013aaa2cf9c4df23f17bc5ae5e
Husniyah <husniyah.cvs@gmail.com>
2024-10-09 13:26:54 -0400

    my second commit it is 1:26pm

--- drivers/screen/colors.c
+++ drivers/screen/colors.c
@@ -1,6 +1,6 @@
 #include <drivers/screen/colors.h>
 //editing this is husniyah
-
+//push using QT creator
 uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
   return fg | (bg << 4);
 }
